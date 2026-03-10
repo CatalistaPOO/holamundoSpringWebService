5d94077ae49bb890f10190ba7265691a63660a74
CatalistaPOO <fgtpoo78@gmail.com>
2026-03-10 22:25:46 +0100

    starting1

--- src/main/java/com/cursospring/holamundoSpringWebService/controller/MutadorController.java
+++ src/main/java/com/cursospring/holamundoSpringWebService/controller/MutadorController.java
@@ -4,7 +4,7 @@ import com.cursospring.holamundoSpringWebService.model.Animal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
-
+//en este caso no es record porque va a cambiar de valor
 public class MutadorController {
     @GetMapping("/mutar")
     public Animal mutar(@RequestBody (required = true) Animal animal){
