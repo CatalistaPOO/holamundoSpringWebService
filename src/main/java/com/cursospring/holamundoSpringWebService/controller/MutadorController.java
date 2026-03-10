832cfa537b5b8c41be6f987f9046b2e3e590e555
CatalistaPOO <fgtpoo78@gmail.com>
2026-03-10 22:34:59 +0100

    nota RestController

--- src/main/java/com/cursospring/holamundoSpringWebService/controller/MutadorController.java
+++ src/main/java/com/cursospring/holamundoSpringWebService/controller/MutadorController.java
@@ -4,7 +4,10 @@ import com.cursospring.holamundoSpringWebService.model.Animal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 //en este caso no es record porque va a cambiar de valor
+@RestController
 public class MutadorController {
     @GetMapping("/mutar")
     public Animal mutar(@RequestBody (required = true) Animal animal){
