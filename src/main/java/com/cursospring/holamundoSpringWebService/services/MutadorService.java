9ab56891e0f1d0920b04c326845f5a3b2a5723df
CatalistaPOO <fgtpoo78@gmail.com>
2026-03-11 17:31:29 +0100

    mutadorService

---
+++ src/main/java/com/cursospring/holamundoSpringWebService/services/MutadorService.java
@@ -0,0 +1,24 @@
+package com.cursospring.holamundoSpringWebService.services;
+
+import com.cursospring.holamundoSpringWebService.model.Animal;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+@Service
+public class MutadorService {
+    public Animal mutar(Animal animal) {
+        List<String> animales = new ArrayList<>();
+        animales.add("Tigre");
+        animales.add("Burro");
+        animales.add("Mosca");
+        animal.setEspecie("Tigre");
+        Random rand = new Random();
+        int indiceAleatorio = rand.nextInt(animales.size());
+        animal.setEspecie(animales.get(indiceAleatorio));
+        return animal;
+    }
+}
+
